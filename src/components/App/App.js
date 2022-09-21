7d5b68457004af6e0a70df52fbab438d17eeed7e
Melton Zheng <meltonzheng@gmail.com>
2022-09-21 00:56:37 -0700

    boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preferences />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>>
+    );
+}
+export default App;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,5 +3,5 @@ import React from 'react';
 export default function Dashboard() {
     return(
         <h2>Dashboard</h2>
-     );
+    );
 }
